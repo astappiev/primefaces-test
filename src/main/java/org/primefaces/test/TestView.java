e66dac083d4d0fa2fd8d75917d05bf56467ba54e
Oleh Astappiev <astappiev@l3s.de>
2020-04-25 20:36:33 +0200

    Example autocomplete with list

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,19 +1,57 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 @Named
 @ViewScoped
 public class TestView implements Serializable {
-    
+
     private String testString;
-    
-    @PostConstruct  
+
+    private List<String> cars;
+    private String[] selectedViaCheckbox;
+    private String[] selectedViaMenu;
+    private String[] selectedViaAutocomplete;
+    private List<String> selectedViaAutocompleteList;
+
+    @PostConstruct
     public void init() {
         testString = "Welcome to PrimeFaces!!!";
+
+        cars = new ArrayList<>();
+        cars.add("Chrysler");
+        cars.add("BMW");
+        cars.add("Mercedes");
+        cars.add("Volkswagen");
+        cars.add("Chrysler");
+        cars.add("Ford");
+        cars.add("Tesla");
+        cars.add("Toyota");
+        cars.add("Hyundai");
+        cars.add("General Motors");
+
+        selectedViaCheckbox = new String[]{"BMW"};
+        selectedViaMenu = new String[]{"Toyota"};
+        selectedViaAutocomplete = new String[]{"Tesla"};
+        selectedViaAutocompleteList = new ArrayList<>(Collections.singletonList("Tesla"));
+    }
+
+    public List<String> completeCar(String query) {
+        String queryLowerCase = query.toLowerCase();
+        return cars.stream().filter(c -> c.toLowerCase().startsWith(queryLowerCase)).collect(Collectors.toList());
+    }
+
+    public void save() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("save."));
     }
 
     public String getTestString() {
@@ -22,5 +60,45 @@ public class TestView implements Serializable {
 
     public void setTestString(String testString) {
         this.testString = testString;
-    }    
+    }
+
+    public List<String> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<String> cars) {
+        this.cars = cars;
+    }
+
+    public String[] getSelectedViaCheckbox() {
+        return selectedViaCheckbox;
+    }
+
+    public void setSelectedViaCheckbox(String[] selectedViaCheckbox) {
+        this.selectedViaCheckbox = selectedViaCheckbox;
+    }
+
+    public String[] getSelectedViaMenu() {
+        return selectedViaMenu;
+    }
+
+    public void setSelectedViaMenu(String[] selectedViaMenu) {
+        this.selectedViaMenu = selectedViaMenu;
+    }
+
+    public String[] getSelectedViaAutocomplete() {
+        return selectedViaAutocomplete;
+    }
+
+    public void setSelectedViaAutocomplete(String[] selectedViaAutocomplete) {
+        this.selectedViaAutocomplete = selectedViaAutocomplete;
+    }
+
+    public List<String> getSelectedViaAutocompleteList() {
+        return selectedViaAutocompleteList;
+    }
+
+    public void setSelectedViaAutocompleteList(List<String> selectedViaAutocompleteList) {
+        this.selectedViaAutocompleteList = selectedViaAutocompleteList;
+    }
 }
