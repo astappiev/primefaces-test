54ccf3d0dd32d75cfc5824bbb91e3a3bf715f43a
Oleh Astappiev <astappiev@l3s.de>
2020-06-17 17:20:44 +0200

    ui:repeat example

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,26 +1,37 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @Named
 @ViewScoped
 public class TestView implements Serializable {
-    
-    private String testString;
-    
+    private static final Logger log = LogManager.getLogger(TestView.class);
+
+    private List<String> list;
+
     @PostConstruct  
     public void init() {
-        testString = "Welcome to PrimeFaces!!!";
+        list = List.of("one", "two", "three");
     }
 
-    public String getTestString() {
-        return testString;
+    public void onSubmit() {
+        log.info("onSubmit()");
     }
 
-    public void setTestString(String testString) {
-        this.testString = testString;
-    }    
+    public List<String> getList() {
+        log.info("getList()");
+        return list;
+    }
+
+    public void setList(final List<String> list) {
+        this.list = list;
+    }
 }
